9b6c785e917f1be7649c43d5c4bddb5378cb7a3e
Ghala Alhrbi <ghala_aalhrbi@hotmail.com>
2025-02-15 15:17:09 +0300

    Update Simple_Calculator.py

--- Python/Simple_Calculator.py
+++ Python/Simple_Calculator.py
@@ -1,1 +1,30 @@
+## The goal is to make a simple calculator
 
+def add(a,b):
+  return a+b
+
+def subtract(a,b):
+  return a-b
+
+def multiply(a,b):
+  return a*b
+
+def divide(a,b):
+  if b == 0:
+    return "cant't divide by 0!"
+  else:
+    return a/b
+
+def square(a):
+  return a*a
+
+def power(a.b):
+  return a**b
+
+def square_root(a):
+  return a**0.5
+
+
+##Test 
+results = add(1, 2)
+print(results)
